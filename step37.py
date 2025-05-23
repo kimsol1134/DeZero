e03300b4e0c862235c54b1e1ba407682095fe276
kimsol1134 <kimsol1134@naver.com>
2025-05-23 09:17:21 +0900

    step37

---
+++ step37.py
@@ -0,0 +1,16 @@
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+c = Variable(np.array([[10, 20, 30], [40, 50, 60]]))
+t = x + c
+y = F.sum(t) # 추후 구현
+
+y.backward(retain_grad=True)
+print(y.grad)
+print(t.grad)
+print(x.grad)
+print(c.grad)
+
+#x.shape == x.grad.shape
