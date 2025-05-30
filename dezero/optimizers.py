21b251dc39d813eb26d584f38e06e96e797a3589
kimsol1134 <kimsol1134@naver.com>
2025-05-30 15:15:28 +0900

    step46

---
+++ dezero/optimizers.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class Optimizer:
+    def __init__(self):
+        self.target = None
+        self.hooks = []
+    
+    def setup(self, target):
+        self.target = target # 매개변수를 갖는 클래스(Model or Layer)를 인스턴스 변수인 target으로 설정
+        return self
+    
+    def update(self):
+        # None 이외의 매개변수를 리스트에 모아둠
+        params = [p for p in self.target.params() if p.grad is not None]
+        # 전처리(옵션)
+        for f in self.hooks:
+            f(params)
+        
+        # 매개변수 갱신
+        for param in params:
+            self.update_one(param)
+        
+    def update_one(self, param): # 구체적인 매개변수 갱신 여기서, Optimizer 자식 클래스에서 재정의 필요
+        raise NotImplementedError()
+    
+    def add_hook(self, f): # 전처리 수행하는 함수 추가 
+        self.hooks.append(f)
+
+
+class SGD(Optimizer):
+    def __init__(self, lr=0.01):
+        super().__init__()
+        self.lr = lr # lr 초기화 
+    def update_one(self, param):
+        param.data -= self.lr * param.grad.data
+
+class MomentumSGD(Optimizer):
+    def __init__(self, lr=0.01, momentum=0.9):
+        super().__init__()
+        self.lr = lr
+        self.momentum = momentum
+        self.vs = {}
+
+    def update_one(self, param):
+        v_key = id(param)
+        if v_key not in self.vs:
+            self.vs[v_key] = np.zeros_like(param.data)
+        
+        v = self.vs[v_key]
+        v *= self.momentum
+        v -= self.lr * param.grad.data
+        param.data += v
