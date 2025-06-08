0345e7f0ce2efb7eb1e0b09ba1fc7c5a845ddd68
kimsol1134 <kimsol1134@naver.com>
2025-06-08 22:17:27 +0900

    step50

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -25,8 +25,8 @@ else:
     from dezero.core import Config
     from dezero.layers import Layer
     from dezero.models import Model
-    # from dezero.datasets import Dataset
-    # from dezero.dataloaders import DataLoader
+    from dezero.datasets import Dataset
+    from dezero.dataloaders import DataLoader
     # from dezero.dataloaders import SeqDataLoader
 
     # import dezero.datasets
--- dezero/functions.py
+++ dezero/functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 from dezero.core import Function
-from dezero.core import as_variable
+from dezero.core import Function, Variable, as_variable, as_array
 from dezero import utils
 
 # =============================================================================
@@ -343,6 +343,22 @@ class SoftmaxCrossEntropy(Function):
 
 def softmax_cross_entropy(x, t):
     return SoftmaxCrossEntropy()(x, t)
+
+
+
+# =============================================================================
+# accuracy / dropout / batch_norm / embed_id
+# =============================================================================
+def accuracy(y, t):
+    """
+    [WAR] This function is not differentiable.
+    """
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
 
 
 # =============================================================================
